b19b3d289fb44b67566454688cd840248dcfa65b
Mariam.Fahim <mariam@fahim.com>
2023-04-16 15:04:36 +0000

    testt

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/register.css') }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,7 +18,8 @@
                     sign up
                     <span class="underline"></span>
                 </button>
-                <form class="form form-login">
+                <form class="form form-login" method="POST" action="{{ route('register') }}">
+                    @csrf
                     <h1>look for a job?</h1>
                     <div class="circle">
                         <img src="{{asset('images/default_prof.png')}}" alt="Profile picture">
@@ -28,22 +31,47 @@
                     <fieldset>
                         <fieldset>
                             <div class="input-block">
-                                <input type="text" id="username" name="username" placeholder="Username" required>
+                                <input type="text" id="name" name="name" placeholder="Username" class="form-control @error('name') is-invalid @enderror" value="{{ old('name') }}" required autocomplete="name">
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="email" id="email" name="email" placeholder="Email" required>
+                                <input type="email" id="email" name="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="text" id="phone" name="phone" placeholder="Phone number" required>
+                                <input type="text" id="phone" name="phone" placeholder="Phone number" class="form-control @error('phone') is-invalid @enderror" value="{{ old('phone') }}" required autocomplete="phone">
+                                @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="password" id="password" name="password" placeholder="Password" required>
+                                <input type="password" id="password" name="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}" required autocomplete="new-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" required>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                             <div class="input-block">
-                                <input type="file" id="cv" name="cv" accept=".pdf,.doc,.docx" placeholder="CV" required>
+                                <input type="file" id="file" name="file" accept=".pdf,.doc,.docx" placeholder="CV" class="form-control @error('file') is-invalid @enderror" value="{{ old('file') }}" required autocomplete="file">
+                                @error('file')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                         </fieldset>
 
@@ -68,25 +96,55 @@
                     <fieldset>
                         <fieldset>
                             <div class="input-block">
-                                <input type="text" id="company_name" name="company_name" placeholder="Company Name" required>
+                                <input type="text" id="company_name" name="company_name" placeholder="Company Name" class="form-control @error('company_name') is-invalid @enderror" value="{{ old('company_name') }}" required autocomplete="company_name">
+                                @error('company_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="text" id="speciality" name="speciality" placeholder="Speciality" required>
+                                <input type="text" id="speciality" name="speciality" placeholder="Speciality" class="form-control @error('speciality') is-invalid @enderror" value="{{ old('speciality') }}" required autocomplete="speciality">
+                                @error('speciality')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="email" id="email" name="email" placeholder="Email" required>
+                                <input type="email" id="email" name="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email') }}" required autocomplete="email">
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="text" id="address" name="address" placeholder="Address" required>
+                                <input type="text" id="address" name="address" placeholder="Address" class="form-control @error('address') is-invalid @enderror" value="{{ old('address') }}" required autocomplete="address">
+                                @error('address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="text" id="phone" name="phone" placeholder="Phone Number" required>
+                                <input type="text" id="phone" name="phone" placeholder="Phone Number" class="form-control @error('phone') is-invalid @enderror" value="{{ old('phone') }}" required autocomplete="phone">
+                                @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="password" id="password" name="password" placeholder="Password" required>
+                                <input type="password" id="password" name="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" value="{{ old('password') }}" required autocomplete="new-password">
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </div>
                             <div class="input-block">
-                                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" required>
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </fieldset>
 
@@ -101,6 +159,12 @@
 
 
 <script src="{{asset('js/register.js')}}"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+    integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+</script>
 
 </body>
 
