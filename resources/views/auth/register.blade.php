51c0e11a8dfe88c58c2e3c3ada196f8a04f115e4
loubna saadi <loubnasaadi88@gmail.com>
2023-04-16 01:29:10 +0000

    css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,11 +9,11 @@
 </head>
 <body>
     <section class="forms-section">
-        <h1 class="section-title">Animated Forms</h1>
+        <h1 class="section-title">Registration</h1>
         <div class="forms">
             <div class="form-wrapper is-active">
                 <button type="button" class="switcher switcher-login">
-                    Login
+                    sign up
                     <span class="underline"></span>
                 </button>
                 <form class="form form-login">
@@ -26,48 +26,71 @@
                             onclick="document.getElementById('profile-picture').click()">Upload</button>
                     </div>
                     <fieldset>
-                        <div class="input-block"><label for="username">Username:</label><br>
-                            <input type="text" id="username" name="username" required>
-                        </div>
-                        <div class="input-block"><label for="email">Email:</label><br>
-                            <input type="email" id="email" name="email" required>
-                        </div>
-                        <div class="input-block"><label for="phone">phone number:</label><br>
-                            <input type="text" id="phone" name="phone" required>
-                        </div>
-                        <div class="input-block"><label for="password">Password:</label><br>
-                            <input type="password" id="password" name="password" required>
-                        </div>
-                        <div class="input-block"><label for="confirm_password">Confirm Password:</label><br>
-                            <input type="password" id="confirm_password" name="confirm_password" required>
-                        </div>
-                        <div class="input-block"><label for="cv">CV:</label><br>
-                            <input type="file" id="cv" name="cv" accept=".pdf,.doc,.docx" required>
-                        </div>
+                        <fieldset>
+                            <div class="input-block">
+                                <input type="text" id="username" name="username" placeholder="Username" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="email" id="email" name="email" placeholder="Email" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="text" id="phone" name="phone" placeholder="Phone number" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="password" id="password" name="password" placeholder="Password" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="file" id="cv" name="cv" accept=".pdf,.doc,.docx" placeholder="CV" required>
+                            </div>
+                        </fieldset>
+
                     </fieldset>
                     <button type="submit" class="btn-login">Register</button>
                 </form>
             </div>
             <div class="form-wrapper">
                 <button type="button" class="switcher switcher-signup">
-                    Sign Up
+                    company
                     <span class="underline"></span>
                 </button>
                 <form class="form form-signup">
+                    <h1>you are a company?</h1>
+                    <div class="circle">
+                        <img src="{{asset('images/default_prof.png')}}" alt="Profile picture">
+                        <input type="file" id="profile-picture" name="photo" accept=".jpg,.jpeg,.png"
+                            style="display: none;">
+                        <button class="upload-btn" type="button"
+                            onclick="document.getElementById('profile-picture').click()">Upload</button>
+                    </div>
                     <fieldset>
-                        <legend>Please, enter your email, password and password confirmation for sign up.</legend>
-                        <div class="input-block">
-                            <label for="signup-email">E-mail</label>
-                            <input id="signup-email" type="email" required>
-                        </div>
-                        <div class="input-block">
-                            <label for="signup-password">Password</label>
-                            <input id="signup-password" type="password" required>
-                        </div>
-                        <div class="input-block">
-                            <label for="signup-password-confirm">Confirm password</label>
-                            <input id="signup-password-confirm" type="password" required>
-                        </div>
+                        <fieldset>
+                            <div class="input-block">
+                                <input type="text" id="company_name" name="company_name" placeholder="Company Name" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="text" id="speciality" name="speciality" placeholder="Speciality" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="email" id="email" name="email" placeholder="Email" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="text" id="address" name="address" placeholder="Address" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="text" id="phone" name="phone" placeholder="Phone Number" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="password" id="password" name="password" placeholder="Password" required>
+                            </div>
+                            <div class="input-block">
+                                <input type="password" id="confirm_password" name="confirm_password" placeholder="Confirm Password" required>
+                            </div>
+                        </fieldset>
+
+
                     </fieldset>
                     <button type="submit" class="btn-signup">Continue</button>
                 </form>
