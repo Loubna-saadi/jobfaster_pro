ed08e1e385c2d628982cbc82f32813bb409eb803
loubna saadi <loubnasaadi88@gmail.com>
2023-04-16 14:26:21 +0000

    f9i9sat

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,73 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <section class="forms-section">
+        <h1 class="section-title">login</h1>
+        <div class="forms">
+            <div class="form-wrapper is-active">
+                <button type="button" class="switcher switcher-login">
+                    user
+                    <span class="underline"></span>
+                </button>
+                <form class="form form-login">
+                    <h1>look for a job?</h1>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                                <fieldset>
+                                  <legend>Please, enter your email and password for login.</legend>
+                                  <div class="input-block">
+                                    <label for="login-email">E-mail</label><br>
+                                    <input id="login-email" type="email" required>
+                                  </div>
+                                  <div class="input-block">
+                                    <label for="login-password">Password</label><br>
+                                    <input id="login-password" type="password" required>
+                                  </div>
+                                </fieldset>
+                                <button type="submit" class="btn-login">Login</button>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                </form>
+            </div>
+            <div class="form-wrapper">
+                <button type="button" class="switcher switcher-signup">
+                    company
+                    <span class="underline"></span>
+                </button>
+                <form class="form form-signup">
+                    <h1>you are a company?</h1>
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                        <fieldset>
+                            <fieldset>
+                                <legend>Please, enter your email and password for login.</legend>
+                                <div class="input-block">
+                                  <label for="login-email">E-mail</label><br>
+                                  <input id="login-email" type="email" required>
                                 </div>
-                            </div>
-                        </div>
+                                <div class="input-block">
+                                  <label for="login-password">Password</label><br>
+                                  <input id="login-password" type="password" required>
+                                </div>
+                              </fieldset>
+                              <button type="submit" class="btn-login">Login</button>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+              </form>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </section>
+
+
+
+<script src="{{asset('js/register.js')}}"></script>
+
+</body>
+
+</html>
